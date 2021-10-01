60bc75fc4732373d5520a36844b66f69aa7db3af
sagorchowdhury <rajchowdhury820@gmail.com>
2021-10-02 01:23:55 +0400

    hello

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1> i am Inventory page</h1>
+
+        </div>
+    );
+};
+
+export default Inventory;
