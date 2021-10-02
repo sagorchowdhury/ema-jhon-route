9021c780862d4edcf6463a58226ece1d0abb0e95
sagorchowdhury <rajchowdhury820@gmail.com>
2021-10-03 00:36:04 +0400

    hello

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import { BrowserRouter as Router}from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Router><
       <Shop></Shop>
+      
     </div>
   );
 }
